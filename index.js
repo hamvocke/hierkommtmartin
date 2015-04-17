b91cf52f6e1241ba9873f563094bbe35c62181d4
Hermann Vocke <hvocke@thoughtworks.com>
2015-04-17 14:24:58 +0200

    switching to websockets

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
-
 
 app.get('/', function (req, res) {
   console.log("GET request received on /");
@@ -16,9 +17,16 @@ app.get('/content', function(req, res) {
 });
 
 app.get('/summer', function(req, res) {
-  res.sendFile(path.join(__dirname + '/public/summer.html'));
+  res.send('Activated summer mode');
 });
 
-app.listen(app.get('port'), function() {
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+http.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
