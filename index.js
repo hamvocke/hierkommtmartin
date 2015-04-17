c511f45c2216a2f764837265a39c59b68f109912
Hermann Vocke <hvocke@thoughtworks.com>
2015-04-17 13:31:57 +0200

    reading port from env

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-var server = app.listen(3000, function () {
-
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
 });
