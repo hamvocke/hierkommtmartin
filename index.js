d9f5418b63054bf386dd9e8bdd2eec24122c04b1
Hermann Vocke <hvocke@thoughtworks.com>
2015-04-17 19:29:29 +0200

    sending tweets when the button has been pressed

--- index.js
+++ index.js
@@ -3,16 +3,38 @@ var app = express();
 var path = require('path');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var Twitter = require('twitter');
 
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
+
+
+var client = new Twitter({
+  consumer_key: 'rQyXZymr8H4FiYMGcwZUMaksP',
+  consumer_secret: 'qvBgUcrEUQ511oftxRjxwjozCCJv6Fi6vABrQyXOhj6fvHNJrk',
+  access_token_key: '3177457618-woh0zfOKhx1Je6r2dZamO1iKtcEfYBfK2aCiU9V',
+  access_token_secret: '5qpK4PZqZxxJPWm04fd3tkhetqa7xlhneRupDcrrgaQnE'
+});
+
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/martin.html'));
 });
 
 app.get('/toggle', function(req, res) {
+  var date = new Date().getTime();
+  console.log(date + ": The Button has been pressed")
   io.emit('season change', 'summer');
+
+  client.post('statuses/update', {status: 'The button has been pressed. #hierkommtmartin #twde'},  function(error, tweet, response){
+    if(error) {
+      console.log("Error occured sending tweet:");
+      console.log(error);
+      console.log(tweet);  // Tweet body.
+      console.log(response);  // Raw response object.
+    }
+  });
+
   res.send('Activated summer mode');
 });
 
